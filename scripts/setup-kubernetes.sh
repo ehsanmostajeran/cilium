9b25e972389c05b677fe7554bc0af187b9664335
André Martins <aanm90@gmail.com>
2015-10-21 00:44:43 +0100

    Fix bug in setup-kubernetes script
    Signed-off-by: André Martins <aanm90@gmail.com>

--- scripts/setup-kubernetes.sh
+++ scripts/setup-kubernetes.sh
@@ -3,6 +3,6 @@ set -e
 dir=`dirname $0`
 
 cd $dir/..
-vagrant ssh -c 'cd cilium/scripts/kubernetes/node1; . ./pre-setup.sh; sudo -E ../master.sh' node1
-vagrant ssh -c 'cd cilium/scripts/kubernetes/node2; . ./pre-setup.sh; sudo -E ../worker.sh' node2
+vagrant ssh -c 'cd cilium/scripts/kubernetes/node1; . ./pre-setup.sh; cp ../master.sh ./ && sudo -E ./master.sh' node1
+vagrant ssh -c 'cd cilium/scripts/kubernetes/node2; . ./pre-setup.sh; cp ../worker.sh ./ && sudo -E ./worker.sh' node2
 echo "Kubernetes dev cluster ready!"
