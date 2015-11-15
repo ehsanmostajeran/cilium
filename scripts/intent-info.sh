16c9cb47fa357392734416d92a89a8009c35a8bc
André Martins <aanm90@gmail.com>
2015-11-15 00:23:13 +0000

    Improved intent-info script
    Signed-off-by: André Martins <aanm90@gmail.com>

--- scripts/intent-info.sh
+++ scripts/intent-info.sh
@@ -6,6 +6,20 @@ if [ -z "${ELASTIC_IP}" ]; then
     ELASTIC_IP=192.168.50.1
 fi
 
-curl -XGET "http://${ELASTIC_IP}:9200/_search?q=${1}&pretty"
+str_search="${1}"
+
+size=100
+if [ "${2}" != "" ]; then
+    size="${2}"
+fi
+
+echo "Logs results:"
+curl -XGET "http://${ELASTIC_IP}:9200/cilium-log*/_search?q=${str_search}&pretty&size=${size}"
+echo ""
+echo "State results:"
+curl -XGET "http://${ELASTIC_IP}:9200/cilium-state/_search?q=${str_search}&pretty&size=${size}"
+echo ""
+echo "Configs results:"
+curl -XGET "http://${ELASTIC_IP}:9200/cilium-configs/_search?q=${str_search}&pretty&size=${size}"
 
 exit 0
