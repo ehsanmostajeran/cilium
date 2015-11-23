1535a68a227be476e22d6b1d5006f774f7461de2
André Martins <aanm90@gmail.com>
2015-11-23 15:45:35 +0000

    Fix entrypoint.sh lack of readability
    Signed-off-by: André Martins <aanm90@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 
 address="https://raw.githubusercontent.com/cilium-team/cilium/master"
 
 dependencies=( \
     "docker" \
-    "docker --version" \
-    "1.8.0" \
-    "Open vSwitch" \
-    "ovs-ofctl --version" \
-    "2.3.2" \
+	"docker --version" \
+	"1.8.0" \
+	"Open vSwitch" \
+	"ovs-ofctl --version" \
+	"2.3.2" \
 )
 
 show_help(){
@@ -41,14 +41,14 @@ show_help(){
 }
 
 check_version(){
-    cmd=$1
-    want=$2
-    [[ $(eval "$cmd") =~ ([0-9][.][0-9][.][0-9]*) ]] && version="${BASH_REMATCH[1]}"
-    local got=$(echo -e "$version\n$want" | sed '/^$/d' | sort -nr | head -1)
-    if [[ "$got" = "$version" ]]; then
+    cmd="${1}"
+    want="${2}"
+    [[ $(eval "${cmd}") =~ ([0-9][.][0-9][.][0-9]*) ]] && version="${BASH_REMATCH[1]}"
+    local got=$(echo -e "${version}\n${want}" | sed '/^$/d' | sort -nr | head -1)
+    if [[ "${got}" = "${version}" ]]; then
         echo "OK!"
     else
-        echo "ERROR: got $version, version $want or higher required"
+        echo "ERROR: got ${version}, version ${want} or higher required"
     fi
 }
 
@@ -57,21 +57,21 @@ check_requirements(){
     for ((i=0; i<"${#dependencies[@]}"; i+=3)); do
         res=$(check_version "${dependencies["$((i+1))"]}" "${dependencies["$((i+2))"]}")
         echo "${dependencies["$((i))"]}: $res"
-        if [ "$res" != "OK!" ]
+        if [ "${res}" != "OK!" ]
         then
             echo "ERROR: Please install the right version of "${dependencies["$((i))"]}""
-            eval "$1=1"
+            eval "${1}=1"
             return
         fi
     done
     docker_IP=$(ip -f inet -o addr show docker0|cut -d\  -f 7 | cut -d/ -f 1)
-    if [[ -z $docker_IP ]]; then
+    if [[ -z "${docker_IP}" ]]; then
         echo "ERROR: Unable to find docker0 IP address."
         echo "Are you sure you have docker daemon running?"
     fi
 
     echo "SUCCESS: All dependencies are available with the right version!"
-    eval "$1=0"
+    eval "${1}=0"
 }
 
 prepare(){
@@ -102,122 +102,122 @@ prepare(){
 
 infect(){
     echo "Infecting node with cilium..."
-    echo "MASTER_IP=$MASTER_IP"
-    echo "IP=$IP"
+    echo "MASTER_IP=${MASTER_IP}"
+    echo "IP=${IP}"
     echo "Downloading scripts..."
     tmp_dir=$(mktemp -d)
-    scripts_tmp="$tmp_dir/scripts"
-    backend_tmp="$tmp_dir/backend"
-    external_deps_temp="$tmp_dir/external-deps"
-    mkdir -p "$scripts_tmp"
-    mkdir -p "$backend_tmp"
-    mkdir -p "$external_deps_temp/docker-collector-configs"
-    curl -Ssl -o "$backend_tmp/setup.sh" "$address/backend/setup.sh"
-    chmod +x "$backend_tmp/setup.sh"
-    curl -Ssl -o "$backend_tmp/config.sh" "$address/backend/config.sh"
-    chmod +x "$backend_tmp/config.sh"
-    curl -Ssl -o "$backend_tmp/utils.sh" "$address/backend/utils.sh"
-    chmod +x "$backend_tmp/utils.sh"
-    curl -Ssl -o "$scripts_tmp/infect-node.sh" "$address/scripts/infect-node.sh"
-    chmod +x "$scripts_tmp/infect-node.sh"
-    curl -Ssl -o "$external_deps_temp/marvel-agent-2.0.0.zip" "$address/external-deps/marvel-agent-2.0.0.zip"
-    curl -Ssl -o "$external_deps_temp/license-2.0.0.zip" "$address/external-deps/license-2.0.0.zip"
-    curl -Ssl -o "$external_deps_temp/docker-collector-configs/templates.json" "$address/external-deps/docker-collector-configs/templates.json"
-    curl -Ssl -o "$external_deps_temp/docker-collector-configs/configs.json" "$address/external-deps/docker-collector-configs/configs.json"
+    scripts_tmp="${tmp_dir}/scripts"
+    backend_tmp="${tmp_dir}/backend"
+    external_deps_temp="${tmp_dir}/external-deps"
+    mkdir -p "${scripts_tmp}"
+    mkdir -p "${backend_tmp}"
+    mkdir -p "${external_deps_temp}/docker-collector-configs"
+    curl -Ssl -o "${backend_tmp}/setup.sh" "${address}/backend/setup.sh"
+    chmod +x "${backend_tmp}/setup.sh"
+    curl -Ssl -o "${backend_tmp}/config.sh" "${address}/backend/config.sh"
+    chmod +x "${backend_tmp}/config.sh"
+    curl -Ssl -o "${backend_tmp}/utils.sh" "${address}/backend/utils.sh"
+    chmod +x "${backend_tmp}/utils.sh"
+    curl -Ssl -o "${scripts_tmp}/infect-node.sh" "${address}/scripts/infect-node.sh"
+    chmod +x "${scripts_tmp}/infect-node.sh"
+    curl -Ssl -o "${external_deps_temp}/marvel-agent-2.0.0.zip" "${address}/external-deps/marvel-agent-2.0.0.zip"
+    curl -Ssl -o "${external_deps_temp}/license-2.0.0.zip" "${address}/external-deps/license-2.0.0.zip"
+    curl -Ssl -o "${external_deps_temp}/docker-collector-configs/templates.json" "${address}/external-deps/docker-collector-configs/templates.json"
+    curl -Ssl -o "${external_deps_temp}/docker-collector-configs/configs.json" "${address}/external-deps/docker-collector-configs/configs.json"
     echo "Downloads completed..."
-    $scripts_tmp/infect-node.sh
+    "${scripts_tmp}/infect-node.sh"
 }
 
 infect_kubernetes(){
     echo "Infecting node with cilium..."
-    echo "MASTER_IP=$MASTER_IP"
-    echo "IP=$IP"
+    echo "MASTER_IP=${MASTER_IP}"
+    echo "IP=${IP}"
     echo "Downloading scripts..."
     export KUBERNETES=true
-    export DOCKER_ENDPOINT="tcp://$IP:2375"
+    export DOCKER_ENDPOINT="tcp://${IP}:2375"
     tmp_dir="$(mktemp -d)"
-    tmp_dir_cilium="$tmp_dir/cilium"
-    scripts_tmp="$tmp_dir_cilium/scripts"
-    scripts_k8s_tmp="$tmp_dir_cilium/scripts/kubernetes"
-    mkdir -p "$tmp_dir_cilium"
-    mkdir -p "$scripts_tmp"
-    mkdir -p "$scripts_k8s_tmp"
-    curl -Ssl -o "$tmp_dir/entrypoint.sh" "$address/entrypoint.sh"
-    chmod +x "$tmp_dir/entrypoint.sh"
-    CILIUM_ROOT=$tmp_dir
-    if [[ -z $MASTER_IP ]]; then
-        curl -Ssl -o "$scripts_k8s_tmp/master.sh" "$address/scripts/kubernetes/master.sh"
-        chmod +x "$scripts_k8s_tmp/master.sh"
+    tmp_dir_cilium="${tmp_dir}/cilium"
+    scripts_tmp="${tmp_dir_cilium}/scripts"
+    scripts_k8s_tmp="${tmp_dir_cilium}/scripts/kubernetes"
+    mkdir -p "${tmp_dir_cilium}"
+    mkdir -p "${scripts_tmp}"
+    mkdir -p "${scripts_k8s_tmp}"
+    curl -Ssl -o "${tmp_dir}/entrypoint.sh" "${address}/entrypoint.sh"
+    chmod +x "${tmp_dir}/entrypoint.sh"
+    CILIUM_ROOT=${tmp_dir}
+    if [[ -z "${MASTER_IP}" ]]; then
+        curl -Ssl -o "${scripts_k8s_tmp}/master.sh" "${address}/scripts/kubernetes/master.sh"
+        chmod +x "${scripts_k8s_tmp}/master.sh"
         echo "Downloads completed..."
-        $scripts_k8s_tmp/master.sh
+        "${scripts_k8s_tmp}/master.sh"
     else
-        curl -Ssl -o "$scripts_k8s_tmp/worker.sh" "$address/scripts/kubernetes/worker.sh"
-        chmod +x "$scripts_k8s_tmp/worker.sh"
+        curl -Ssl -o "${scripts_k8s_tmp}/worker.sh" "${address}/scripts/kubernetes/worker.sh"
+        chmod +x "${scripts_k8s_tmp}/worker.sh"
         echo "Downloads completed..."
-        $scripts_k8s_tmp/worker.sh
+        "${scripts_k8s_tmp}/worker.sh"
     fi
 }
 
 start_services(){
     echo "Downloading scripts..."
     tmp_dir=$(mktemp -d)
-    scripts_tmp="$tmp_dir/scripts"
-    mkdir -p "$scripts_tmp"
-    curl -Ssl -o "$scripts_tmp/setup-haproxy.sh" "$address/scripts/setup-haproxy.sh"
-    chmod +x "$scripts_tmp/setup-haproxy.sh"
-    curl -Ssl -o "$scripts_tmp/setup-dns.sh" "$address/scripts/setup-dns.sh"
-    chmod +x "$scripts_tmp/setup-dns.sh"
-    curl -Ssl -o "$scripts_tmp/setup-services.sh" "$address/scripts/setup-services.sh"
-    chmod +x "$scripts_tmp/setup-services.sh"
+    scripts_tmp="${tmp_dir}/scripts"
+    mkdir -p "${scripts_tmp}"
+    curl -Ssl -o "${scripts_tmp}/setup-haproxy.sh" "${address}/scripts/setup-haproxy.sh"
+    chmod +x "${scripts_tmp}/setup-haproxy.sh"
+    curl -Ssl -o "${scripts_tmp}/setup-dns.sh" "${address}/scripts/setup-dns.sh"
+    chmod +x "${scripts_tmp}/setup-dns.sh"
+    curl -Ssl -o "${scripts_tmp}/setup-services.sh" "${address}/scripts/setup-services.sh"
+    chmod +x "${scripts_tmp}/setup-services.sh"
     echo "Downloads completed..."
-    $scripts_tmp/setup-services.sh
+    "${scripts_tmp}/setup-services.sh"
     echo "Done"
 }
 
-requote() { sed 's/[^\/]/&/g; s/\//\\\//g' <<< "$1"; }
+requote() { sed 's/[^\/]/&/g; s/\//\\\//g' <<< "${1}"; }
 
 store_policy(){
-    echo "Using network address (NET_IP): $NET_IP"
+    echo "Using network address (NET_IP): ${NET_IP}"
     echo "Downloading basic policies..."
     tmp_dir=$(mktemp -d)
-    policies_tmp="$tmp_dir/policy"
-    mkdir -p "$policies_tmp"
-    curl -Ssl -o "$policies_tmp/1-cluster.yml" "$address/policy/1-cluster.yml"
-    curl -Ssl -o "$policies_tmp/2-dns-config.yml" "$address/policy/2-dns-config.yml"
-    curl -Ssl -o "$policies_tmp/3-haproxy-config.yml" "$address/policy/3-haproxy-config.yml"
-    curl -Ssl -o "$policies_tmp/4-debug-shell.yml" "$address/policy/4-debug-shell.yml"
-    net_ip=$(requote "$NET_IP")
-    sed -i "s/192.168.50.0\/24/$net_ip/g" "$policies_tmp/1-cluster.yml"
+    policies_tmp="${tmp_dir}/policy"
+    mkdir -p "${policies_tmp}"
+    curl -Ssl -o "${policies_tmp}/1-cluster.yml" "${address}/policy/1-cluster.yml"
+    curl -Ssl -o "${policies_tmp}/2-dns-config.yml" "${address}/policy/2-dns-config.yml"
+    curl -Ssl -o "${policies_tmp}/3-haproxy-config.yml" "${address}/policy/3-haproxy-config.yml"
+    curl -Ssl -o "${policies_tmp}/4-debug-shell.yml" "${address}/policy/4-debug-shell.yml"
+    net_ip=$(requote "${NET_IP}")
+    sed -i "s/192.168.50.0\/24/${net_ip}/g" "${policies_tmp}/1-cluster.yml"
     docker_IP=$(ip -f inet -o addr show docker0|cut -d\  -f 7 | cut -d/ -f 1)
-    sed -i "s/172.17.0.1/$docker_IP/g" "$policies_tmp/1-cluster.yml"
+    sed -i "s/172.17.0.1/${docker_IP}/g" "${policies_tmp}/1-cluster.yml"
     docker run --rm \
-        -e ELASTIC_IP=$IP \
-        -v "$policies_tmp":/opt/cilium/policies/ \
-        cilium/cilium -f /opt/cilium/policies
+           -e ELASTIC_IP="${IP}" \
+           -v "${policies_tmp}":/opt/cilium/policies/ \
+           cilium/cilium -f /opt/cilium/policies
 }
 
 start_kibana(){
     tmp_dir=$(mktemp -d)
-    scripts_tmp="$tmp_dir/scripts"
-    backend_tmp="$tmp_dir/backend"
-    external_deps_temp="$tmp_dir/external-deps"
-    curl -Ssl -o "$external_deps_temp/marvel-2.0.0.tar.gz" "$address/external-deps/marvel-2.0.0.tar.gz"
+    scripts_tmp="${tmp_dir}/scripts"
+    backend_tmp="${tmp_dir}/backend"
+    external_deps_temp="${tmp_dir}/external-deps"
+    curl -Ssl -o "${external_deps_temp}/marvel-2.0.0.tar.gz" "${address}/external-deps/marvel-2.0.0.tar.gz"
     echo "Starting Kibana..."
     docker run \
-        --name cilium-kibana \
-        -e ELASTICSEARCH_URL=http://$IP:9200 \
-        -p 5601:5601 \
-        -d kibana:4.2.0
-    docker cp "$external_deps_temp/marvel-2.0.0.tar.gz" cilium-kibana:/tmp
+           --name "cilium-kibana" \
+           -e ELASTICSEARCH_URL="http://${IP}:9200" \
+           -p 5601:5601 \
+           -d kibana:4.2.0
+    docker cp "${external_deps_temp}/marvel-2.0.0.tar.gz" cilium-kibana:/tmp
     docker exec -ti cilium-kibana kibana plugin --install marvel --url file:///tmp/marvel-2.0.0.tar.gz
     docker restart cilium-kibana
 }
 
 entry(){
-    case $1 in
+    case "${1}" in
         check)
             check_requirements ret
-            if [[ "$ret" == 0 ]]; then
+            if [[ "${ret}" == 0 ]]; then
                 exit 0
             else
                 exit 1
@@ -228,20 +228,20 @@ entry(){
             exit 0
             ;;
         infect)
-            if [[ -z $NET_IP ]] && [[ -z $MASTER_IP ]]; then
+            if [[ -z "${NET_IP}" ]] && [[ -z "${MASTER_IP}" ]]; then
                 echo "ERROR: NET_IP or MASTER_IP is empty"
                 show_help
                 exit 0
             fi
-            if [[ -z $IP ]]; then
+            if [[ -z "${IP}" ]]; then
                 echo "ERROR: IP is empty"
                 show_help
                 exit 0
             fi
             check_requirements ret
-            if [[ "$ret" == 0 ]]; then
+            if [[ "${ret}" == 0 ]]; then
                 infect
-                if [[ -n $NET_IP ]]; then
+                if [[ -n "${NET_IP}" ]]; then
                     store_policy
                     start_services
                     echo "First node infected!"
@@ -256,20 +256,20 @@ entry(){
             exit 0
             ;;
         infect-kubernetes)
-            if [[ -z $NET_IP ]] && [[ -z $MASTER_IP ]]; then
+            if [[ -z "${NET_IP}" ]] && [[ -z "${MASTER_IP}" ]]; then
                 echo "ERROR: NET_IP or MASTER_IP is empty"
                 show_help
                 exit 0
             fi
-            if [[ -z $IP ]]; then
+            if [[ -z "${IP}" ]]; then
                 echo "ERROR: IP is empty"
                 show_help
                 exit 0
             fi
             check_requirements ret
-            if [[ "$ret" == 0 ]]; then
+            if [[ "${ret}" == 0 ]]; then
                 infect_kubernetes
-                if [[ -n $NET_IP ]]; then
+                if [[ -n "${NET_IP}" ]]; then
                     echo "Master node infected!"
                 else
                     echo "Worker node infected!"
@@ -282,7 +282,7 @@ entry(){
             exit 0
             ;;
         start-kibana)
-            if [[ -z $IP ]]; then
+            if [[ -z "${IP}" ]]; then
                 echo "ERROR: IP is empty"
                 show_help
                 exit 0
@@ -297,4 +297,4 @@ entry(){
     esac
 }
 
-entry "$@"
+entry "${@}"
